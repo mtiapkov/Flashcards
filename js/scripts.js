3d3a656f6ddb6649195d961bf707a5db9ea36655
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-11 16:33:15 -0700

    Implement working JS
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+$("#car").click(function() {
+  $("#box1").toggle();
+})
+
+$("#bike").click(function() {
+  $("#box2").toggle();
+})
+
+$("#skate").click(function() {
+  $("#box3").toggle();
+})
+
+})
